d4c37e413095a2f315b53573ba5d197595de1403
Reznnov <64481216+Reznnov@users.noreply.github.com>
2020-11-05 10:42:01 +0300

    aaaaaaaaasdasdasda

--- ft_rev_par_list.py
+++ ft_rev_par_list.py
@@ -1,5 +1,12 @@
+def ft_len(str):
+    num = 0
+    for i in str:
+        num += 1
+    return num
+
+
 def ft_rev_list(mass):
-    dlin = len(mass)
+    dlin = ft_len(mass)
     for i in range(dlin - 1, -1, -1):
         mass.append(mass.pop(i))
     return mass
@@ -7,13 +14,34 @@ def ft_rev_list(mass):
 
 def ft_rev_par_list(mass):
     spisok = []
-    dlin = len(mass)
+    dlin = ft_len(mass)
     per = 0
     while per <= dlin:
-        if per < dlin:
-            spisok.append(mass[per + 1])
-            spisok.append(mass[per])
-            per += 2
+        if dlin % 2 == 0:
+            if per < dlin:
+                spisok.append(mass[per + 1])
+                spisok.append(mass[per])
+                if dlin % 2 == 0:
+                    per += 2
+                else:
+                    if dlin > per + 3:
+                        per += 2
+                        spisok.append(mass[per])
+                        per += 1000
+                    else:
+                        per += 2
+            else:
+                per += 1000
         else:
-            per += 1000
-    return ft_rev_list(spisok)
+            if per < dlin:
+                spisok.append(mass[per + 1])
+                spisok.append(mass[per])
+                if dlin > per + 3:
+                    per += 2
+                else:
+                    per += 2
+                    spisok.append(mass[per])
+                    per += 1000
+            else:
+                per += 1000
+    return spisok

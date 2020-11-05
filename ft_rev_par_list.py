8fbb8c2aa72cf94812d0d8677ef1d0524148b005
Reznnov <64481216+Reznnov@users.noreply.github.com>
2020-11-05 09:56:33 +0300

    sdfsf

--- ft_rev_par_list.py
+++ ft_rev_par_list.py
@@ -1,16 +1,19 @@
+def ft_rev_list(mass):
+    dlin = len(mass)
+    for i in range(dlin - 1, -1, -1):
+        mass.append(mass.pop(i))
+    return mass
+
+
 def ft_rev_par_list(mass):
     spisok = []
     dlin = len(mass)
     per = 0
     while per <= dlin:
-        print(per)
-        print(dlin)
         if per < dlin:
             spisok.append(mass[per + 1])
             spisok.append(mass[per])
             per +=2
-            print(spisok)
-            print(per)
         else:
             per += 1000
-    return spisok[::-1]
+    return ft_rev_list(spisok)
